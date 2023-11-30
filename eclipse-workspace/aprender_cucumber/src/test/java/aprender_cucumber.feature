c0e824b820519fff0cbe8e9ac8c0d55e2c2743eb
Felipe Carneiro <correaflp@gmail.com>
2023-11-30 18:55:53 -0300

    primeiro teste ok

---
+++ eclipse-workspace/aprender_cucumber/src/test/java/aprender_cucumber.feature
@@ -0,0 +1,14 @@
+
+# encoding: UTF-8
+
+@tag
+Feature: Aprender Cucumber
+	Como um aluno
+	Eu quero aprender a utilizar Cucumber
+	Para que eu possa automatizar criterios de aceitacao
+	
+	@tag1
+	Scenario: Deve executar especificacao
+		Given que crei o arquivo corretamente
+		When executa-lo
+		Then a especificacao deve finalizar com sucesso
---
+++ eclipse-workspace/aprender_cucumber/src/test/java/aprender_cucumber/AprenderCucumber.java
@@ -0,0 +1,23 @@
+package aprender_cucumber;
+
+import io.cucumber.java.en.*;
+
+public class AprenderCucumber {
+	@Given("que crei o arquivo corretamente")
+	public void que_crei_o_arquivo_corretamente() {
+	    // Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@When("executa-lo")
+	public void executa_lo() {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
+
+	@Then("a especificacao deve finalizar com sucesso")
+	public void a_especificacao_deve_finalizar_com_sucesso() {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
+}
