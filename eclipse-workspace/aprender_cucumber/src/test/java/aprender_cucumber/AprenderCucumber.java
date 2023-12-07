062e07b8f7ef82da16404a0713e0c9f2270fd34e
Felipe Carneiro <correaflp@gmail.com>
2023-12-07 09:51:48 -0300

    pt-br (mesmo codigo)

--- eclipse-workspace/aprender_cucumber/src/test/java/aprender_cucumber/AprenderCucumber.java
+++ eclipse-workspace/aprender_cucumber/src/test/java/aprender_cucumber/AprenderCucumber.java
@@ -1,21 +1,26 @@
 package aprender_cucumber;
 
-import io.cucumber.java.en.*;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+//import io.cucumber.java.pt.*;
+
 
 public class AprenderCucumber {
-	@Given("que crei o arquivo corretamente")
+	@Dado("que crei o arquivo corretamente")
 	public void que_crei_o_arquivo_corretamente() {
 	    // Write code here that turns the phrase above into concrete actions
 
 	}
 
-	@When("executa-lo")
+	@Quando("executa-lo")
 	public void executa_lo() {
 	    // Write code here that turns the phrase above into concrete actions
 	   
 	}
 
-	@Then("a especificacao deve finalizar com sucesso")
+	@Então("a especificacao deve finalizar com sucesso")
 	public void a_especificacao_deve_finalizar_com_sucesso() {
 	    // Write code here that turns the phrase above into concrete actions
 	   
