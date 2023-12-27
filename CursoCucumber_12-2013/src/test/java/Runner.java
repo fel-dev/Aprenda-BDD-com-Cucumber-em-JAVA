ff8e884400a11d17690235618f06043a50c10f74
Felipe Carneiro <correaflp@gmail.com>
2023-12-27 02:31:49 -0300

    rodou, mas ta melhor q antes
    io.cucumber.junit.Cucumber

--- CursoCucumber_12-2013/src/test/java/Runner.java
+++ CursoCucumber_12-2013/src/test/java/Runner.java
@@ -1,7 +1,12 @@
+//import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 
 import cucumber.api.junit.Cucumber;
 
+
+//import io.cucumber.junit.Cucumber;
+
+//@ExtendWith(Cucumber.class)
 @RunWith(Cucumber.class)
 public class Runner {
 
