1e040e6b9932930ffdb27966177c53aa803959f5
Felipe Carneiro <correaflp@gmail.com>
2023-12-27 20:37:06 -0300

    Exprecoes Regulares
    12. Reaproveitando cenários - REGEX
    
    (\d+\/\d+\/\d+)
    e
    ^que o prazo eh dia (\d)/(\d)/(\d)$
    em https://jex.im/regulex
    
    (\d+\/\d+\/\d+)
    em https://regex101.com/
    
    Explication
    https://www.udemy.com/course/cucumber-java/learn/lecture/9923390#questions
    
    GPT - shared chat about regex
    https://chat.openai.com/share/1bb2f0db-2ef8-49ca-a9f0-74b3c144f067

--- CursoCucumber_12-2013/src/test/java/AprenderCucumber.java
+++ CursoCucumber_12-2013/src/test/java/AprenderCucumber.java
@@ -1,3 +1,6 @@
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Assert;
 
 import io.cucumber.java.pt.Dado;
@@ -6,35 +9,59 @@ import io.cucumber.java.pt.Então;
 
 public class AprenderCucumber {
 
-//	public void que_crei_o_arquivo_corretamente() throws Throwable {
-	@Dado("que crei o arquivo corretamente")
-	public void queCreiOArquivoCorretamente() {
-	}
+    @Dado("^que crei o arquivo corretamente$")
+    public void queCreiOArquivoCorretamente() throws Throwable {
+    }
 
-	@Quando("executa-lo")
-	public void executaLo() {
-	}
+    @Quando("^executa-lo$")
+    public void executaLo() throws Throwable {
+    }
 
-	@Então("a especificacao deve finalizar com sucesso")
-	public void aEspecificacaoDeveFinalizarComSucesso() {
-	}
-	
-	private int contador = 0;
-	
-	@Dado("que o valor do contador é {int}")
-	public void queOValorDoContadorÉ(Integer int1) throws Throwable{
-	    contador = int1;
-	}
+    @Então("^a especificacao deve finalizar com sucesso$")
+    public void aEspecificacaoDeveFinalizarComSucesso() throws Throwable {
+    }
 
-	@Quando("eu incrementar em {int}")
-	public void euIncrementarEm(Integer int1) throws Throwable{
-	    contador = contador + int1;
-	}
+    private int contador = 0;
 
-	@Então("o valor do contador será {int}")
-	public void oValorDoContadorSerá(Integer int1) throws Throwable{
-	    Assert.assertEquals((Integer)int1, (Integer)contador);
-		new RuntimeException();
-	}
+    @Dado("^que o valor do contador é (\\d+)$")
+    public void queOValorDoContadorÉ(Integer int1) throws Throwable {
+        contador = int1;
+    }
+
+    @Quando("^eu incrementar em (\\d+)$")
+    public void euIncrementarEm(Integer int1) throws Throwable {
+        contador = contador + int1;
+    }
+
+    @Então("^o valor do contador será (\\d+)$")
+    public void oValorDoContadorSerá(Integer int1) throws Throwable {
+        Assert.assertEquals((Integer) int1, (Integer) contador);
+    }
+    
+    
+    Date entrega = new Date();
+    
+    @Dado("^que o prazo é dia (\\d+)/(\\d+)/(\\d+)$")
+    public void queOPrazoÉDia(Integer dia, Integer mes, Integer ano) throws Throwable {
+       Calendar cal = Calendar.getInstance();
+       cal.add(Calendar.DAY_OF_MONTH, dia);
+       cal.add(Calendar.MONTH, mes);
+       cal.add(Calendar.YEAR, ano);
+       entrega = cal.getTime();
+    }
+
+    @Quando("^a entrega atrasar em (\\d+) dias$")
+    public void aEntregaAtrasarEmDias(Integer int1) throws Throwable {
+    	Calendar cal = Calendar.getInstance();
+        cal.setTime(entrega);
+        cal.add(Calendar.DAY_OF_MONTH, (Integer) int1);
+        entrega = cal.getTime();
+    }
+
+    @Então("^a entrega será efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+    public void aEntregaSeráEfetuadaEm(String data) throws Throwable  {
+        // Write code here that turns the phrase above into concrete actions
+//        throw new cucumber.api.PendingException();
+    }
 
 }
--- CursoCucumber_12-2013/src/test/java/aprender_cucumber.feature
+++ CursoCucumber_12-2013/src/test/java/aprender_cucumber.feature
@@ -15,3 +15,13 @@ Funcionalidade: Aprender Cucumber
 		Dado que o valor do contador é 15
 		Quando eu incrementar em 3
 		Então o valor do contador será 18
+		
+	Cenário: Deve incrementar contador
+		Dado que o valor do contador é 123
+		Quando eu incrementar em 35
+		Então o valor do contador será 158
+		
+	Cenário: Deve calcular atraso no praso de entrega
+		Dado que o prazo é dia 05/04/2018
+		Quando a entrega atrasar em 2 dias
+		Então a entrega será efetuada em 07/04/2018
