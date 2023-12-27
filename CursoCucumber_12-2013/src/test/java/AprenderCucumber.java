f119ed235b42709e615439c33ac71099c4f4330d
Felipe Carneiro <correaflp@gmail.com>
2023-12-26 23:30:35 -0300

    Create AprenderCucumber.java

---
+++ CursoCucumber_12-2013/src/test/java/AprenderCucumber.java
@@ -0,0 +1,23 @@
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AprenderCucumber {
+
+	@Given("^que crei o arquivo corretamente$")
+	public void que_crei_o_arquivo_corretamente() throws Throwable {
+	    
+	}
+
+	@When("^executa-lo$")
+	public void executa_lo() throws Throwable {
+	    
+	}
+
+	@Then("^a especificacao deve finalizar com sucesso$")
+	public void a_especificacao_deve_finalizar_com_sucesso() throws Throwable {
+	    
+	}
+	
+}
