1825d825c959c89332016e993b697c985857abcb
Felipe Carneiro <correaflp@gmail.com>
2023-12-27 16:10:55 -0300

    11. Asserts
    Linkar o conteúdo ao JUnit

--- CursoCucumber_12-2013/src/test/java/AprenderCucumber.java
+++ CursoCucumber_12-2013/src/test/java/AprenderCucumber.java
@@ -1,3 +1,5 @@
+import org.junit.Assert;
+
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Quando;
 import io.cucumber.java.pt.Então;
@@ -20,20 +22,25 @@ public class AprenderCucumber {
 	private int contador = 0;
 	
 	@Dado("que o valor do contador é {int}")
-	public void queOValorDoContadorÉ(Integer int1) {
+	public void queOValorDoContadorÉ(Integer int1) throws Throwable{
 	    contador = int1;
+//	    System.out.println("Contador agora é: " + contador);
 	}
 
 	@Quando("eu incrementar em {int}")
-	public void euIncrementarEm(Integer int1) {
+	public void euIncrementarEm(Integer int1) throws Throwable{
 	    contador = contador + int1;
+//	    System.out.println("Contador agora é: " + contador);
+//	    System.out.println("Incremento é: " + int1);
 	}
 
 	@Então("o valor do contador será {int}")
-	public void oValorDoContadorSerá(Integer int1) {
-	    System.out.println(int1);
-	    System.out.println(contador);
+	public void oValorDoContadorSerá(Integer int1) throws Throwable{
+		System.out.println("Incremento é: " + int1);
+	    System.out.println("Contador agora é: " + contador);
 	    System.out.println(int1 == contador);
+//	    Assert.assertTrue(int1 == contador);
+	    Assert.assertEquals(int1, contador);
 	}
 
 }
